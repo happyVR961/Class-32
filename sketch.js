cd17f2c248a0a743a7c63f25f7ab1cb56de009d5
happyVR961 <happyVR961@gmail.com>
2020-07-23 18:56:50 -0700

    added timezone

--- sketch.js
+++ sketch.js
@@ -5,13 +5,15 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1,pig3;
-var backgroundImg,platform;
+var bgimage,platform;
 var bird, slingshot;
-
+var count = 0;
 var gameState = "onSling";
+var bg = "sprites/bg.png";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    
+    happy();  
 }
 
 function setup(){
@@ -45,18 +47,26 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if (bgimage){
+    background(bgimage);
+    }
     Engine.update(engine);
+    textSize(35);
+    fill("white");
+    text("Score:"+count,900,40);
+    
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -66,7 +76,8 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display();
+      
 }
 
 function mouseDragged(){
@@ -85,4 +96,22 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function happy(){
+var response = await fetch("http://worldtimeapi.org/api/timezone/America/Mexico_City");
+var rjson = await response.json();
+//console.log(rjson);
+var dt = rjson.datetime;
+//console.log(dt);
+var hour = dt.slice(11,13);
+//console.log(hour);
+if (hour>06 && hour<17){
+    bg = "sprites/bg.png";
+}
+else {
+    bg = "sprites/bg2.jpg";
+}
+bgimage = loadImage(bg);
+
 }
